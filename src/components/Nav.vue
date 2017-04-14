ebb6636f77d8a80d4b26392d63d6e5ee7bed024c
xiaobai <948206388@qq.com>
2017-04-14 16:58:37 +0800

    add icon-less and logo

---
+++ src/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav">
+  	<div class="container">
+  		<div class="nav-logo">
+  			<i class="icon ion-radio-waves"></i>
+  		</div>
+  		<div class="nav-nav float-left">1</div>
+  		<div class="nav-nav float-right">2</div>
+  	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
