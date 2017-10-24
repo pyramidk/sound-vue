aca7ae767c96ae8e14323629e18ebca2580d9730
pyramidk <948206388@qq.com>
2017-10-24 22:12:22 +0800

    fix little error

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-  divs(class="toolbar")
-    divs(class="container")
-      divs(class="toolbar-items")
+  div(class="toolbar")
+    div(class="container")
+      div(class="toolbar-items")
         <router-link to="/songs/chill" class="toolbar-item toolbar-genre" :class="{'router-link-active': typeNow === 'chill'}">悠然</router-link>
         <router-link to="/songs/deep" class="toolbar-item toolbar-genre">深邃</router-link>
         <router-link to="/songs/dubstep" class="toolbar-item toolbar-genre">贝斯</router-link>
@@ -10,7 +10,7 @@
         <router-link to="/songs/tech" class="toolbar-item toolbar-genre">质感</router-link>
         <router-link to="/songs/trance" class="toolbar-item toolbar-genre">迷幻</router-link>
         <router-link to="/songs/tropical" class="toolbar-item toolbar-genre">舒适</router-link>
-        divs(class="toolbar-item toolbar-filter toolbar-times")
+        div(class="toolbar-item toolbar-filter toolbar-times")
           i(class="icon ion-funnel")
           a(class="toolbar-time " href="") 7 days
           a(class="toolbar-time ") 30 days
